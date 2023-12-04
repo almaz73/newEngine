980600964a6fef05605273dede7bef03a31a2077
a.fayzrakhmanov <a.fayzrakhmanov@cartat.ru>
2023-12-04 16:51:27 +0300

    autorization fix

--- src/stores/_g_axios.ts
+++ src/stores/_g_axios.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
+import {useGlobalStore} from "@/stores/globalStore";
+import router from "@/router";
 
 axios.interceptors.response.use(resp => resp
     , err => {
         console.log('Перехватываею все ошибкиь err=', err)
         console.log(" err.request.status = ", err.request.status)
+
+        if (!err.request.status && err.code === "ERR_NETWORK") {
+            // возможно у токена вышел срок, перекидываем на cтраницу авторизации
+            useGlobalStore().isAuthorized = false
+            router.push('login')
+        }
         return err
     })
 
