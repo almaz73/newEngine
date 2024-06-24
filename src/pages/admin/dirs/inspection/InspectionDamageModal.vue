7f5ac956d169f07d57223deb638c831f44c0a6fa
a.fayzrakhmanov <a.fayzrakhmanov@cartat.ru>
2024-06-24 16:29:56 +0300

    Повреждения

--- src/pages/admin/dirs/inspection/InspectionDamageModal.vue
+++ src/pages/admin/dirs/inspection/InspectionDamageModal.vue
@@ -3,16 +3,19 @@
               @closeModal="closeModal()"
               :width="globalStore.isMobileView? 330: 480"
               :top="40"
-              :title="'Элемент осмотра'"
+              :title="'Повреждения'"
               draggable
               resizable>
       <el-scrollbar maxHeight="480px">
-  
+
         <span class="modal-fields">
-          <small>
-            <label class="label-right l_150">Провреждения:</label>
-            <el-input title="Провреждения" v-model="insp.damageName"/>
-          </small>
+           <el-form ref="form" :model="insp" class="error-to-message">
+            <el-form-item prop="damageName" :rules="{required: true, message: 'Название повреждения', trigger: ['change']}">
+              <label class="label l_200">Название повреждения</label>
+              <el-input v-model="insp.damageName"/>
+            </el-form-item>
+           </el-form>
+          <el-divider/>
           <div style="text-align: right">
             <el-button type="danger" @click="save()" :icon="Plus">Сохранить</el-button>
             <el-button type="info" @click="isOpen = false">Отмена</el-button>
@@ -33,38 +36,32 @@
   import {ref} from "vue";
   import {Plus} from "@element-plus/icons-vue";
   import {ElMessage} from "element-plus";
-  import UsersDirModal_History from "@/pages/admin/dirs/UsersDirModal_History.vue";
-  import {inspectionItemCategories, inspectionUiType} from "@/utils/globalConstants";
+  import {checkEmptyFields} from "@/utils/globalFunctions";
   
   const globalStore = useGlobalStore();
   const isOpen = ref(false);
-  const insp = ref({});
   const closeModal = () => isOpen.value = false;
   const adminStore = useAdminStore();
+  const insp = ref({})
+  const form = ref(null)
   let cb;
   
   
   function open(row, cbModal) {
     cb = cbModal;
     isOpen.value = true;
-    if (!row) model.value = {damageName:''};
+    if (!row) insp.value = {damageName:''}
     else insp.value = JSON.parse(JSON.stringify(row))
   }
-  
-  
-  function checking() {
-    if (!insp.value.damageName) {
-      return ElMessage({message: 'Поле "Повреждение" обязателен для заполнения', type: "warning"});
-    }
-  }
-  
+
   function save() {
-    if (checking()) return false;
-    adminStore.saveDamage(insp.value).then(() => {
-      ElMessage({message: "Осмотр успешно сохранен", type: "success"});
-      isOpen.value = false;
-      cb();
-    });
+    checkEmptyFields(form.value).then(res => { // проверка заполненности обязательных полей
+      res && adminStore.saveDamage(insp.value).then(() => {
+        ElMessage({message: "Осмотр успешно сохранен", type: "success"});
+        isOpen.value = false;
+        cb();
+      });
+    })
   }
   
   defineExpose({open});
