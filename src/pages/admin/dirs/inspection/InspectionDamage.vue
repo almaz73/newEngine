acf380ffcd96ca129d7e8e4f850aa2679db5768f
damikmaxxx <damik200355555@mail.ru>
2024-06-24 12:30:40 +0300

    Админка/ Лист осмотра. Добавлена папка под табы , добавлена страница с повреждениями , работает создание, редактирование и удаление

---
+++ src/pages/admin/dirs/inspection/InspectionDamage.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="admin-filter-field">
+      <el-input v-model="like"
+                :prefix-icon="Search"
+                placeholder="Фильтр"
+                @clear="like=''"
+                clearable
+                :style="{marginRight: globalStore.isMobileView?'80px':'30px'}"
+                @input="toSearch()"/>
+      <el-button @click="openModal()" type="danger" :icon="Plus"> Добавить</el-button>
+      <br><br>
+      <el-table v-if="!globalStore.isMobileView"
+                :data="tableData"
+                style="width: 100%; margin-bottom: 20px"
+                :default-sort="{ prop: 'order', order: 'descending'}"
+                highlight-current-row>
+        <el-table-column prop="damageName" label="Название" sortable/>
+        <el-table-column prop="roleTitle" width="73px">
+          <template #default="scope">
+            <div style="" class="admin-table-editors">
+              <img @click="openModal(scope.row)" alt=""
+                   title="Редактировать"
+                   src="@/assets/icons/icon-pencil-gray.png">
+              <img @click="deleteInsp(scope.row.id)" alt=""
+                   src="@/assets/icons/icon-cross-gray.png"
+                   title="Удалить">
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+  
+      <div class="vertical-table" v-if="globalStore.isMobileView">
+        <div v-for="(row, ind) in tableData" :key="ind" style="border-top:8px solid #ddd">
+          <span>{{ row.name }}
+             <el-button>
+               <img @click="openModal(row)" alt=""
+                    title="Редактировать"
+                    src="@/assets/icons/icon-pencil-gray.png">
+             </el-button>
+          </span>
+          <div><small>Категория:</small> {{ row.inspectionItemCategory }}</div>
+          <div><small>Порядок:</small> {{ row.order }}</div>
+        </div>
+      </div>
+    </div>
+    <InspectionDamageModal ref="InspectionModal"/>
+  </template>
+  <script setup lang="ts">
+  import {useAdminStore} from "@/stores/adminStore";
+  import {ref} from "vue";
+  import {ElMessage, ElMessageBox, ElTable} from "element-plus";
+  import {useGlobalStore} from "@/stores/globalStore";
+  import InspectionDamageModal from "@/pages/admin/dirs/inspection/InspectionDamageModal.vue";
+  import {Plus, Search} from "@element-plus/icons-vue";
+  import {inspectionItemCategories} from "@/utils/globalConstants"
+  const globalStore = useGlobalStore()
+  const adminStore = useAdminStore()
+  const tableData = ref([])
+  let tableDataMemory = []
+  const isEdit = ref(false)
+  const selectedRow = ref(false)
+  const InspectionModal = ref(null)
+  const like = ref('')
+  
+  function openModal(row: any | null) {
+    InspectionModal.value.open(row, getData)
+  }
+  
+  
+  function deleteInsp(id: number) {
+    ElMessageBox.confirm('Вы действительно хотите удалить запись?', 'Внимание', {
+      confirmButtonText: 'Да',
+      cancelButtonText: 'Нет'
+    })
+        .then((res) => {
+          res && adminStore.deleteDamage(id).then(() => {
+            ElMessage({message: 'Осмотр успешно удален', type: 'success'})
+            getData()
+          })
+        })
+  }
+  
+  function toSearch() {
+    let word = like.value.toUpperCase()
+    tableData.value = tableDataMemory.filter(el => el.damageName.toUpperCase().includes(word))
+  }
+  
+  
+  function getData() {
+    isEdit.value = false
+    selectedRow.value = false
+    adminStore.getDomage().then(res => {
+      tableData.value = res.items
+      tableDataMemory = JSON.parse(JSON.stringify(res.items))
+    })
+  }
+  function open(){
+    globalStore.setTitle('Повреждения')
+    globalStore.steps = []
+    getData()
+  }
+
+  defineExpose({open});
+  
+  </script>
---
+++ src/pages/admin/dirs/inspection/InspectionTabs.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-tabs @tab-click="tabchange" v-model="activeName">
+      <el-tab-pane label="Осмотр" name="first">
+        <InspectionView/>
+      </el-tab-pane>
+      <el-tab-pane label="Повреждение">
+        <InspectionDamage ref="viewRef"/>
+      </el-tab-pane>
+  
+    </el-tabs>
+  </template>
+  <script setup lang="ts">
+  import InspectionView from "@/pages/admin/dirs/inspection/InspectionView.vue"
+  import InspectionDamage from "@/pages/admin/dirs/inspection/InspectionDamage.vue"
+  import {ref} from "vue";
+  
+  const activeName = ref('first')
+  const viewRef = ref(null);
+  function tabchange(val: any) {
+
+    if (val.props.label === 'Повреждение') viewRef.value.open()
+  }
+  </script>
