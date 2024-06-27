17d0d077ddd94301e7fa0aac14869942a6e27564
damikmaxxx <damik200355555@mail.ru>
2024-06-27 15:19:41 +0300

    Добавлена Админка/ Кредитные программы

---
+++ src/pages/admin/dirs/CreditProgramDir.vue
@@ -0,0 +1,91 @@
+<template>
+    <div>
+      <div style="margin-bottom: 30px">
+        <el-input v-model="search"
+                  :prefix-icon="Search"
+                  @clear="find()"
+                  clearable
+                  :style="{marginRight: globalStore.isMobileView?'80px':'30px'}"
+                  @input="find()"/>
+        <el-button @click="openModal()"
+                   type="danger" :icon="Plus">{{ globalStore.isMobileView ? '' : 'Добавить' }}
+        </el-button>
+      </div>
+  
+      <el-table
+          :data="tableData"
+          empty-text="Нет данных"
+          highlight-current-row>
+        <el-table-column label="Название" prop="name" sortable />
+        <el-table-column label="Процентная ставка" prop="creditPercent" sortable />
+        <el-table-column label="Банк" prop="bankItem.name"  />
+        <el-table-column width="73">
+          <template #default="scope">
+            <div style="" class="admin-table-editors">
+            <img @click="openModal(scope.row)" alt=""
+                 title="Редактировать"
+                 src="@/assets/icons/icon-pencil-gray.png">
+            <img @click="deleteRow(scope.row)" alt=""
+                 src="@/assets/icons/icon-cross-gray.png"
+                 title="Удалить">
+                 </div>
+          </template>
+        </el-table-column>
+
+      </el-table>
+    </div>
+    <CreditProgramDirModal ref="InspectionModal"/>
+  </template>
+  <script setup lang="ts">
+  import {useAdminStore} from "@/stores/adminStore";
+  import {ref} from "vue";
+  import {ElMessage, ElMessageBox, ElTable} from "element-plus";
+  import {useGlobalStore} from "@/stores/globalStore";
+  import {Search, Plus} from '@element-plus/icons-vue'
+  import CreditProgramDirModal from '@/pages/admin/dirs/CreditProgramDirModal.vue'
+  const globalStore = useGlobalStore()
+  const adminStore = useAdminStore()
+  const tableData = ref([])
+  const InspectionModal = ref(null)
+  let tableDataMemory = []
+  const search = ref('')
+  const selectedRow = ref(false)
+
+
+  
+  function find() {
+    tableData.value = tableDataMemory.filter(el => el.name.toUpperCase().includes(search.value.toUpperCase()))
+    if (!search.value) tableData.value = tableDataMemory
+  }
+  
+
+  function deleteRow(row: any) {
+    ElMessageBox.confirm('Вы действительно хотите удалить?', 'Внимание', {
+      confirmButtonText: 'Да',
+      cancelButtonText: 'Нет'
+    })
+        .then(() => {
+          adminStore.deleteCreditProgram(row.id).then(() => {
+            ElMessage({message: 'Строка удалена.', type: 'success'})
+            getData()
+          })
+        })
+  }
+    
+  function openModal(row: any | null) {
+  InspectionModal.value.open(row, getData)
+}
+  function getData() {
+    selectedRow.value = false
+    adminStore.getCreditProgram().then(res => {
+      tableData.value = res.items
+      tableDataMemory = JSON.parse(JSON.stringify(res.items))
+    })
+  }
+  
+  globalStore.setTitle('Кредитные программы')
+  globalStore.steps = []
+  getData()
+  
+  
+  </script>
---
+++ src/pages/admin/dirs/CreditProgramDirModal.vue
@@ -0,0 +1,121 @@
+<template>
+    <AppModal v-if="isOpen"
+              @closeModal="closeModal()"
+              :width="340"
+              :top="40"
+              :title="'Кредитная программа'"
+              draggable>
+      <el-scrollbar maxHeight="480px">
+        <el-form ref="form" :model="model" class="error-to-message">
+          <span class="modal-field">
+            <el-form-item prop="name" :rules="{required: true, message: 'Название', trigger: ['change']}">
+              <label class="label-right l_100">Название</label>
+              <el-input
+                  style="width: 190px"
+                  v-model="model.name"
+                  clearable
+                  placeholder="Название">
+             </el-input>
+          </el-form-item>
+          <el-form-item prop="creditPercent" :rules="{required: true, message: 'Процентная ставка', trigger: ['change']}">
+              <label class="label-right l_100">Процентная ставка</label>
+              <el-input
+                  style="width: 190px"
+                  v-model="model.creditPercent"
+                  clearable
+                  placeholder="Процентная ставка">
+             </el-input>
+          </el-form-item>
+          <el-form-item prop="bankItem.bank.bankId" :rules="{required: true, message: 'Банк партнер', trigger: ['change']}">
+           <label class="label-right l_100">Банк партнер</label>
+           <el-select
+            v-model="model.bankItem.bank.bankId"
+            style="width: 190px"
+            @change="changeBank"
+            clearable
+          >
+            <el-option
+              v-for="item in banks"
+              :key="item.bankId"
+              :label="item.name"
+              :value="item.bankId"
+            />
+          </el-select> 
+        </el-form-item> 
+        <el-form-item prop="bankItemId" :rules="{required: true, message: 'Филиал', trigger: ['change']}">
+           <label class="label-right l_100">Филиал</label>
+           <el-select
+            v-model="model.bankItemId"
+            style="width: 190px"
+          >
+            <el-option
+              v-for="item in filials"
+              :key="item.bankItemId"
+              :label="item.name"
+              :value="item.bankItemId"
+            />
+          </el-select> 
+        </el-form-item> 
+          <span class="modal-buttons-bottom">
+            <el-button type="danger" @click="save()" :icon="Plus">Сохранить</el-button>
+            <el-button type="info" @click="isOpen = false">Отмена</el-button>
+          </span>
+
+          </span>
+          </el-form>
+      </el-scrollbar>
+    </AppModal>
+  </template>
+  
+  <script setup>
+  import AppModal from "@/components/AppModal.vue";
+  import {useAdminStore} from "@/stores/adminStore";
+  import {ref} from "vue";
+  import {Plus} from "@element-plus/icons-vue";
+  import {ElMessage} from "element-plus";
+  import {checkEmptyFields} from "@/utils/globalFunctions";
+  const isOpen = ref(false);
+  const model = ref({});
+  const banks = ref([])
+  const filials = ref([])
+  const form = ref(null)
+  const closeModal = () => isOpen.value = false;
+  const adminStore = useAdminStore();
+  let cb;
+  
+  function open(row, cbModal) {
+    cb = cbModal;
+    isOpen.value = true;
+    if(!row)model.value.bankItem = {bank:{bankName:null,bankId:null}}
+    else{
+        model.value = JSON.parse(JSON.stringify(row))
+        changeOrg(model.value.bankItemId)
+    }
+    adminStore.getBanks().then(res => {
+        console.log(res.items)
+        banks.value = res.items
+    })
+  }
+
+  function changeBank(id){
+    model.value.bankItemId = null
+    if(id){
+        adminStore.getBankFilials2(id).then(res => {
+        filials.value = res.items
+    })
+    }
+
+  }
+  function save() {
+    checkEmptyFields(form.value).then(res => {
+      res && adminStore.saveCreditProgram(model.value, 20).then(() => {
+      ElMessage({message: "Запись успешно сохранена", type: "success"});
+      isOpen.value = false;
+      cb();
+    });
+    }) 
+  
+  }
+  
+  defineExpose({open});
+  </script>
