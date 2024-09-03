96b0a2ea7a28868d1133ce8265001fd05aa7ed29
almaz <almaz_73@mail.ru>
2024-09-03 10:03:19 +0300

    rf prewiev auto-photo

--- src/components/UploadPhotoAuto.vue
+++ src/components/UploadPhotoAuto.vue
@@ -28,18 +28,25 @@
     <img @click="rotatePhoto(20)" alt="" src="@/assets/icons/rotateRight.png">
     <img @click="rotatePhoto(10)" alt="" src="@/assets/icons/rotateLeft.png">
   </div>
+  <Teleport to="body">
+    <el-dialog v-model="dialogVisible" top="12px"
+               style="display:table;text-align:center;width:300px">
+      <div>{{ PhotoNumberBuyer[nextNumber] }}</div>
+      <a :href="props.listBigPictures[nextNumber]+isDirty" target="_blank"
+         style="position:absolute;right:40px;top:14px;color:#bbb"> Открыть в отдельной вкладке</a>
+      <img v-if="props.photo && props.number===nextNumber"
+           :src="props.listBigPictures[nextNumber]+isDirty" alt=""
+           style="max-height:800px" />
+      <img v-if="props.photo && props.number!==nextNumber"
+           :src="props.listBigPictures[nextNumber]" alt=""
+           style="max-height: 800px" />
+      <div style="margin-bottom: -10px">
+        <el-button @click="nextPhoto(-1)"> <</el-button>
+        <el-button @click="nextPhoto(1)"> ></el-button>
+      </div>
+    </el-dialog>
 
-  <el-dialog v-model="dialogVisible" top="0" draggable :width="globalStore.isMobileView?'100%':'73%'">
-    {{ PhotoNumberBuyer[nextNumber] }}
-    <img v-if="props.photo && props.number===nextNumber"
-         :src="props.listBigPictures[nextNumber]+isDirty" alt=""
-         style="max-height: 800px" />
-    <img v-if="props.photo && props.number!==nextNumber"
-         :src="props.listBigPictures[nextNumber]" alt=""
-         style="max-height: 800px" />
-    <el-button @click="nextPhoto(-1)"> < </el-button>
-    <el-button @click="nextPhoto(1)"> > </el-button>
-  </el-dialog>
+  </Teleport>
 </template>
 
 
@@ -66,8 +73,10 @@ const nextNumber = ref(props.number)
 function nextPhoto(val) {
   let numbers = Object.keys(props.listBigPictures)
 
+  // todo numbers не всегда содержит только необходимые для показа фотки, но и прикрепленные
+
   nextNumber.value = +numbers[numbers.lastIndexOf('' + nextNumber.value) + val]
-  if (!nextNumber.value) nextNumber.value = +numbers[0]
+  if (isNaN(nextNumber.value)) nextNumber.value = +numbers[0]
 }
 
 function newFile() {
@@ -82,15 +91,13 @@ const handlePictureCardPreview = () => {
 
 function keyHear(key) {
   if (key.key === 'ArrowLeft') nextPhoto(-1)
-  if (key.key === 'ArrowRight')nextPhoto(1)
+  if (key.key === 'ArrowRight') nextPhoto(1)
 }
 
 watch(dialogVisible, (val) => {
   if (val) document.addEventListener('keydown', keyHear)
   if (!val) document.removeEventListener('keydown', keyHear)
 })
-
-
 
 
 const deleteFile = () => {
