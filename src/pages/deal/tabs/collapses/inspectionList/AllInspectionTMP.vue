639d694ca63e89010a4e8ca2c881404daf995579
a.fayzrakhmanov <a.fayzrakhmanov@cartat.ru>
2024-09-12 08:12:11 +0300

    inspection list editor

--- src/pages/deal/tabs/collapses/inspectionList/AllInspectionTMP.vue
+++ src/pages/deal/tabs/collapses/inspectionList/AllInspectionTMP.vue
@@ -5,37 +5,76 @@
         <div style="max-width: 350px ;float: left">
           <span v-if="!['20'].includes(categoryId)">
             <el-icon style="color: green" v-if="item.isNorm">
-              <CircleCheckFilled />
+              <CircleCheckFilled/>
             </el-icon>
             <el-icon style="color: #f56c6c" v-if="!item.isNorm">
-              <RemoveFilled />
+              <RemoveFilled/>
             </el-icon>
           </span>
           <span v-else>
-            <el-icon  style="color: #777" v-if="item.isStock">
-              <CircleCheck />
+            <el-icon style="color: #777" v-if="item.isStock">
+              <CircleCheck/>
             </el-icon>
             <el-icon style="color: #eee" v-if="!item.isStock">
-              <RemoveFilled />
+              <RemoveFilled/>
             </el-icon>
           </span>
 
           {{ item.name }}
         </div>
 
-        <div style="float: right; cursor: pointer"
+
+        <div v-if="['30'].includes(categoryId)"
+             style="float: right; cursor: pointer"
              @click="changeItem(item)"
              @mouseover="item.isNorm=!item.isNorm"
              @mouseleave="toMouseLeave(item)"
         >
-          <el-button v-if="!['20'].includes(categoryId)"
-            :type="!item.isNorm?'danger':'success'" style="width: 150px; pointer-events:none">
+        &nbsp; &nbsp;<el-button
+              :type="!item.isNorm?'danger':'success'"
+              style="width: 150px; pointer-events:none; width: 111px">
             <span>  {{ item.isNorm ? 'Норма' : 'Не норма !' }}</span>
+          </el-button>
+        </div>
 
+
+        <div v-if="['30'].includes(categoryId)"
+             style="float: right; cursor: pointer"
+             @click="changeItem(item, 'isRepaired')"
+             @mouseover="item.isRepaired=!item.isRepaired"
+             @mouseleave="toMouseLeave(item)"
+        >
+          <el-button
+              :style="{'background' :item.isRepaired?'orange':'#c6e0cc'}"
+              style="width: 150px; pointer-events:none; width: 112px">
+            <span>  {{ !item.isRepaired ? 'Оригинал' : 'Не оригинал !' }}</span>
+          </el-button>
+        </div>
+
+
+
+
+        <div v-if="!['20', '30'].includes(categoryId) "
+             style="float: right; cursor: pointer"
+             @click="changeItem(item)"
+             @mouseover="item.isNorm=!item.isNorm"
+             @mouseleave="toMouseLeave(item)"
+        >
+          <el-button
+              :type="!item.isNorm?'danger':'success'" style="width: 150px; pointer-events:none">
+            <span>  {{ item.isNorm ? 'Норма' : 'Не норма !' }}</span>
           </el-button>
 
-          <el-button v-else
-            :style="{'background' :!item.isStock?'#eee':'#c6e0cc'}" style="width: 150px; pointer-events:none">
+
+        </div>
+        <div  v-if="['20'].includes(categoryId)"
+             style="float: right; cursor: pointer"
+             @click="changeItem(item, 'isStock')"
+             @mouseover="item.isStock=!item.isStock"
+             @mouseleave="toMouseLeaveStock(item, 'isStock')"
+        >
+          <el-button
+              :style="{'background' :!item.isStock?'#eee':'#c6e0cc'}" style="width: 150px; pointer-events:none">
             <span>  {{ item.isStock ? 'Присутствует' : 'Отсутствует' }}</span>
 
           </el-button>
@@ -43,54 +82,50 @@
 
         <div style="clear: both"></div>
 
-        <div v-if="!['20'].includes(categoryId) && dangerField[item.id]" style="display: flex" >
+        <div v-if="!['20'].includes(categoryId) && dangerField[item.id]" style="display: flex">
 
-          <div style="display: flex; gap: 12px; float: left;  min-width: 282px" >
-            <UploadPhoto v-if="item.photos[0]" @setNewPhoto="setNewPhoto" :url="item.photos[0].previewPhotoPath" />
-            <UploadPhoto v-if="item.photos[1]" @setNewPhoto="setNewPhoto" :url="item.photos[1].previewPhotoPath" />
-            <UploadPhoto v-if="!item.photos[0]" @setNewPhoto="setNewPhoto" />
-            <UploadPhoto v-if="item.photos[0] && !item.photos[1]" @setNewPhoto="setNewPhoto" />
+          <div style="display: flex; gap: 12px; float: left;  min-width: 282px; margin-top: 4px">
+            <UploadPhoto v-if="item.photos[0]" @setNewPhoto="setNewPhoto" :url="item.photos[0].previewPhotoPath"/>
+            <UploadPhoto v-if="item.photos[1]" @setNewPhoto="setNewPhoto" :url="item.photos[1].previewPhotoPath"/>
+            <UploadPhoto v-if="!item.photos[0]" @setNewPhoto="setNewPhoto"/>
+            <UploadPhoto v-if="item.photos[0] && !item.photos[1]" @setNewPhoto="setNewPhoto"/>
           </div>
 
           <div>
             <el-select
 
-              placeholder="можно несколько"
-              style="width: 238px; padding: 4px 0"
-              v-model="item.damageTypeArr"
-              multiple
-              filterable>
+                placeholder="можно несколько"
+                style="width: 238px; padding: 4px 0"
+                v-model="item.damageTypeArr"
+                multiple
+                filterable>
               <el-option v-for="item in item.damageItems"
                          :key="item.id"
                          :label="item.damageName"
-                         :value="item.id" />
+                         :value="item.id"/>
             </el-select>
 
             <div style="clear: both"></div>
           </div>
         </div>
-        <el-divider />
+        <el-divider/>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useGlobalStore } from '@/stores/globalStore'
-import { ref } from 'vue'
+import {useGlobalStore} from '@/stores/globalStore'
+import {ref} from 'vue'
 import UploadPhoto from '@/components/UploadPhoto.vue'
-import { CircleCheckFilled, RemoveFilled , CircleCheck} from '@element-plus/icons-vue'
+import {CircleCheck, CircleCheckFilled, RemoveFilled} from '@element-plus/icons-vue'
 
 const globalStore = useGlobalStore()
-const auto = ref({})
 const dangerField = ref({})
-const chapok = ref(false)
-const dealId = ref(null)
-const autoId = ref(null)
-const inspectionId = ref(null)
+const chapok = ref(false) // помощник при нажатиии кнопок
 const listData = ref([])
 
-const props=defineProps(['categoryId'])
+const {categoryId} = defineProps(['categoryId'])
 
 
 function setNewPhoto(val) {
@@ -98,22 +133,36 @@ function setNewPhoto(val) {
 }
 
 
-function toMouseLeave(item) {
+function toMouseLeave(item, type) {
   if (!chapok.value) item.isNorm = !item.isNorm
   chapok.value = false
 }
 
 
-function changeItem(item) {
-  if (chapok.value) item.isNorm = !item.isNorm
-  dangerField.value[item.id] = !item.isNorm
+
+function toMouseLeaveStock(item) {
+  if (!chapok.value) item.isStock = !item.isStock
+  chapok.value = false
+}
+
+function changeItem(item, type) {
+  if (type === 'isNorm') {
+    if (chapok.value) item.isNorm = !item.isNorm
+    dangerField.value[item.id] = !item.isNorm
+  } else if (type === 'isRepaired') {
+    if (chapok.value) item.isRepaired = !item.isRepaired
+    // dangerField.value[item.id] = !item.isRepaired
+  } else if (type === 'isStock') {
+    if (chapok.value) item.isStock = !item.isStock
+    // dangerField.value[item.id] = !item.isRepaired
+  }
   chapok.value = true
 }
 
 function open(_listData) {
   console.log('TMP')
   listData.value = _listData
-  console.log('listData.value = ',listData.value)
+  console.log('listData.value = ', listData.value)
   listData.value.map(el => {
     dangerField.value[el.id] = !el.isNorm // раскрываем поля с ошибками
     if (el.damageType) el.damageTypeArr = el.damageType.split(',').map(Number) // список повреждений
@@ -121,5 +170,5 @@ function open(_listData) {
   })
 }
 
-defineExpose({ open })
+defineExpose({open})
 </script>
